043fa1bedc5c17e14629af2f1619e7b1f8b0ddb4
tomo.tomo <tomotomo@gmail.com>
2020-08-19 10:05:56 +0000

    tasklist validates_partial

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence:true
   validates :status, presence: true, length: { maximum: 10 }
   validates :content, presence: true, length: { maximum: 255 }
 end
